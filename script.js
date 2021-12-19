a95384938052b04b0ba0730de6b31391340ee4b3
Jordan Rothwell <jordanlrothwell@gmail.com>
2021-12-19 12:39:53 +1000

    Created arrays for keeping track of which questions have already been asked; attempting to integrate them into existing code.

--- script.js
+++ script.js
@@ -10,63 +10,92 @@
 // WHEN the game is over
 // THEN I can save my initials and my score
 
-// Start button 
+// Start button
 
 // Timer
-    // Lose time if answer incorrect
+// Lose time if answer incorrect
 
 // Questions
-    // Multiple choice
-    // Random
-    // Content = javascript
+// Multiple choice
+// Random
+// Content = javascript
 
 // Score
-    // Persists between sessions
+// Persists between sessions
 
 // PLACEHOLDER QUESTIONS
 // TODO: Organise these somewhere else, preferably in a JSON file
 var question1 = {
-    question: "is this a functioning object?",
-    options: ["yes", "no", "ayeeeeeee", "ducks"],
-    correct: 0
-}
+  question: "is this a functioning object?",
+  options: ["yes", "no", "ayeeeeeee", "ducks"],
+  correct: 0,
+};
 
 var question2 = {
-    question: "how about this one",
-    options: ["yeeeeees", "noooooooooo"],
-    correct: 1
-}
+  question: "how about this one",
+  options: ["yeeeeees", "noooooooooo"],
+  correct: 1,
+};
 
-var questions = [question1, question2]
+var question3 = {
+  question: "oooooooooh",
+  options: ["boiiiiiiii", "noooooo", "my gawd"],
+  correct: 2,
+};
+
+var question4 = {
+  question: "is this the",
+  options: ["real life", "thing i wanted to be doing today", "quickie mart"],
+  correct: 1,
+};
+
+var questions = [question1, question2, question3, question4];
 
 // Getting elements from the doc
 var questionEl = document.getElementById("question");
 var optionsEl = document.getElementById("options");
 var optionListEl = document.getElementById("optionList");
 const startButton = document.querySelector("button");
-var timerEl = document.getElementById("timer")
+var timerEl = document.getElementById("timer");
 
 var t = 60;
 
-// Defining functions
+var previousQuestionIndexes = [];
+var currentQuestionIndex = [];
+var remainingQuestionIndexes = [];
 
-var chooseRandom = function(arr) {
-    return arr[Math.floor(Math.random() * arr.length)]
-}
-
-var questionAppender = function(chosenQuestion) {
-    for (var i = 0; i < chosenQuestion.options.length; i++) {
-        var newListElement = document.createElement("li");
-        var optionText = document.createTextNode(chosenQuestion.options[i]);
-        newListElement.appendChild(optionText);
-        document.getElementById("optionList").appendChild(newListElement)
+var initialiseRemainingQuestionIndexes = function(arr) {
+    for (var i = 0; i < arr.length; i++) {
+        remainingQuestionIndexes = remainingQuestionIndexes.concat(i)
     }
 }
 
-var presentQuestion = function() {
-    var chosenQuestion = questions[Math.floor(Math.random() * (questions.length))]
-    questionEl.textContent = chosenQuestion.question;
-    questionAppender(chosenQuestion);
+var chooseNewIndex = function() {
+    var newIndex = remainingQuestionIndexes[Math.floor(Math.random() * remainingQuestionIndexes)];
+    var indexOfIndex = remainingQuestionIndexes.indexOf[newIndex];
+    remainingQuestionIndexes.splice(indexOfIndex);
+    currentQuestionIndex = currentQuestionIndex.concat(newIndex)
 }
 
-presentQuestion()
+initialiseRemainingQuestionIndexes();
+chooseNewIndex
+
+
+// Defining functions
+
+var chooseRandom = function (arr) {
+  return Math.floor(Math.random() * arr.length);
+};
+
+var setQuestionText = function (Q) {
+  questionEl.textContent = Q.question;
+};
+
+var setOptionsText = function (Q) {
+  for (var i = 0; i < Q.options.length; i++) {
+    var newListElement = document.createElement("li");
+    var optionText = document.createTextNode(Q.options[i]);
+    newListElement.appendChild(optionText);
+    document.getElementById("optionList").appendChild(newListElement);
+  }
+};
